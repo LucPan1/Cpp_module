18f578d673d84b4314fa104ce35ac00c1d419a9f
LucPan1 <lucpan2003@gmail.com>
2025-12-02 00:41:22 +0100

    feat: add accessors and start of add

--- cpp_00/ex01/PhoneBook.hpp
+++ cpp_00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 17:01:25 by lupan             #+#    #+#             */
-/*   Updated: 2025/11/30 21:30:13 by luc              ###   ########.fr       */
+/*   Updated: 2025/12/01 18:34:19 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,14 @@ class PhoneBook {
     public:
         PhoneBook( void );
         ~PhoneBook( void );
-        void    add_contact();
+
+        int getFirstName( void ) const;
+        int getLastName( void ) const;
+        int getNickname( void ) const;
+        int getPhoneNumber( void ) const;
+        int getDarkestSecret( void ) const;
+        void    add_contact(std::string first_name, std::string last_name, std::string nickname, 
+            std::string phone_number, std::string darkest_secret);
 
     private:
         Contact _contacts[8];
--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 14:54:10 by lupan             #+#    #+#             */
-/*   Updated: 2025/11/30 21:33:49 by luc              ###   ########.fr       */
+/*   Updated: 2025/11/30 21:43:50 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ int main()
     // PhoneBook   instance;
     int	command;
 
+	command = 0;
 	while (command != 3)
 	{
 		std::cout << "1. ADD  2. SEARCH  3. EXIT" << std::endl;
 		std::cin >> command;
-		if (command == 1)
-		{
+		// if (command == 1)
+		// {
 			
-		}
+		// }
 	}
     return (0);
 }
