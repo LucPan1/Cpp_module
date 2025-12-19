edcc58810c80e7bc0310587c471be5e92bf27f63
LucPan1 <lucpan2003@gmail.com>
2025-12-19 23:15:18 +0100

    fix: megaphone

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:28:34 by lupan             #+#    #+#             */
-/*   Updated: 2025/12/19 00:59:16 by luc              ###   ########.fr       */
+/*   Updated: 2025/12/19 23:14:16 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 
 int	main(int ac, char **av)
 {
-	if (ac < 2)
-	{
+	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+	} else {
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j] != '\0'; j++)
+				av[i][j] = static_cast<unsigned char>(std::toupper(av[i][j]));
+			std::cout << av[i];
+		}
+		std::cout << std::endl;
 	}
-
-	for (int i = 1; i < ac; i++)
-	{
-		for (int j = 0; av[i][j] != '\0'; j++)
-			av[i][j] = static_cast<unsigned char>(std::toupper(av[i][j]));
-		std::cout << av[i];
-		std::cout << " ";
-	}
-	std::cout << std::endl;
 	return (0);
 }
