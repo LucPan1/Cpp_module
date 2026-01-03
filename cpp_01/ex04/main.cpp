b9cf4d093d3e8e9a6bb62d1f77c0f406ff742714
LucPan1 <lucpan2003@gmail.com>
2026-01-03 19:47:45 +0100

    feat: replace any string

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -3,99 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupan <lupan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 15:41:42 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/02 16:44:48 by lupan            ###   ########.fr       */
+/*   Updated: 2026/01/03 19:43:49 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "File.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int ft_replace(char **av)
+{
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::string	line;
+	size_t 		i = 0;
+	std::size_t	pos;
+	std::fstream filein;
+	std::ofstream out_file;
+
+	filein.open(av[1], std::ios::in);
+	if (filein.fail())
+	{
+		std::cout << "Error: " << av[1] << ":" <<
+		" no such file or directory" << std::endl;
+		return (1);
+	}
+	out_file.open((std::string(av[1]) + ".replace").c_str(), std::ios::out);
+	if (out_file.fail())
+		return (1);
+	while (std::getline(filein, line)) {
+		i = 0;
+		while (i < line.length())
+		{
+			pos = line.find(s1);
+			if (pos != std::string::npos)
+			{
+				line.erase(pos, s1.length());
+				line.insert(pos, s2);
+			}
+			out_file << line[i];
+			i++;
+			if (i == line.length())
+				out_file << std::endl;
+		}
+	}
+	out_file.close();
+	filein.close();
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
 	if (ac != 4)
 		std::cout << "Wrong arguments output" << std::endl;
 	else
-	{
-		std::string s1 = av[2];
-		std::string s2 = av[3];
-		std::string	line;
-		// std::string rep;
-		// std::string cmp;
-	
-		// size_t i = 0;
-		// size_t j = 0;
-		// size_t k = 0;
-		// size_t count = 0;
-		std::fstream filein(av[1], std::ios::in);
-	
-		std::ofstream out_file("file.replace", std::ios::out);
-		if (filein && out_file) {
-			while (std::getline(filein, line)) {
-				// while (i < line.length())
-				// {
-					std::size_t found = line.find(s1);
-					if (found != std::string::npos)
-						std::cout << "second 'needle' found at: " << found << '\n';
-				// 	else
-				// 	{
-				// 		out_file << line[i];	
-				// 	}
-					
-				// }
-			}
-			std::cout << "Copy Finished \n";
-		}
-		else {
-			std::cout << "Error cannot read File" << std::endl;
-		}
-		return 0;
-	}
+		ft_replace(av);
+	return (0);
 }
-
-// i = 0;
-// j = 0;
-// k = 0;
-// count = 0;
-// std::cout << "passe" << std::endl;
-// while (i < line.length())
-// {
-	
-// }
-// rep = line[i];
-// cmp = s1[j];
-// if (rep.compare(cmp) == 0)
-// {
-// 	line[i] = s2[k];
-// 	out_file << line[i];
-// 	std::cout << "line[" << i << "]:" << line[i] << std::endl;
-// 	i++;
-// 	if (j < s1.length() - 1)
-// 		j++;
-// 	std::cout << "s1.length: " << s1.length() << std::endl;
-// 	std::cout << "s2.length: " << s2.length() << std::endl;
-// 	std::cout << "k: " << k << std::endl;
-
-// 	if (k < s2.length() - 1)
-// 		k++;
-// 	if (k == s2.length())
-// 	{
-// 		std::cout << "heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" << std::endl;
-// 		std::cout << "k: " << k << " " << "s2: " << s2.length() << std::endl;
-// 		while (j < s1.length())
-// 		{
-// 			j++;
-// 			count++;
-// 			std::cout << "j: " << j << std::endl;
-// 		}
-// 		i += count;
-
-// 	}
-// 	continue;
-// }
-// out_file << line[i];
-// std::cout << "line[" << i << "]: " << line[i] << std::endl;
-// i++;
-// if (i == line.length())
-// 	out_file << std::endl;
