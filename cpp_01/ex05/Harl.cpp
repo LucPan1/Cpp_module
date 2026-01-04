6400ccf80fb3154c16abdc853398a59077744518
LucPan1 <lucpan2003@gmail.com>
2026-01-04 23:51:19 +0100

    refactor: levels

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 20:14:16 by luc               #+#    #+#             */
-/*   Updated: 2026/01/04 22:42:32 by luc              ###   ########.fr       */
+/*   Updated: 2026/01/04 23:50:05 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,26 +25,27 @@ Harl::~Harl()
 }
 
 void	Harl::debug(void) {
-    std::cout << "I love having extra bacon for my" 
+    std::cout << "[ DEBUG ]" << std::endl << "I love having extra bacon for my" 
 	<< "7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
     << std::endl;
 }
 
 void	Harl::info(void) {
-	std::cout << "I cannot believe adding extra bacon costs more money."
-	<< "You didn’t put enough bacon in my burger! If you did, I wouldn’t"
+	std::cout << "[ INFO ]" << std::endl << "I cannot believe adding extra bacon costs more money." << std::endl
+	<< "You didn’t put enough bacon in my burger! If you did, I wouldn’t "
 	<< "be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void) {
-	std::cout << "I think I deserve to have some extra bacon for free."
-	<< "I’ve been coming for years, whereas you started working here just"
+	std::cout << "[ WARNING ]" << std::endl << "I think I deserve to have some extra bacon for free." 
+	<< std::endl << "I’ve been coming for years, whereas you started working here just "
 	<< "last month." << std::endl;
 }
 
 void	Harl::error(void) {
-	std::cout << "This is unacceptable! I want to speak to the manager now."
-			  << std::endl;
+	std::cout << "[ ERROR ]" << std::endl 
+	<< "This is unacceptable! I want to speak to the manager now."
+	<< std::endl;
 }
 
 void	Harl::complain(std::string level) {
