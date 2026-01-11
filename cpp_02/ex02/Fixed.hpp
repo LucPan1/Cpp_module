e9b9f73b939578d76ab785bf2a5dcdbdefc80310
LucPan1 <lucpan2003@gmail.com>
2026-01-11 20:26:22 +0100

    feat: add operators

---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 16:32:19 by luc               #+#    #+#             */
+/*   Updated: 2026/01/11 20:20:19 by luc              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+    public:
+        Fixed();
+        Fixed(const int fixed_point_number);
+        Fixed(const double fixed_point_number);
+        Fixed(const Fixed& other);
+        Fixed &operator=(const Fixed& other);
+        ~Fixed();
+
+        int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+        float	toFloat(void) const;
+        int		toInt(void) const;
+
+		int operator>(const Fixed &fixed);
+		int operator<(const Fixed &fixed);
+		int operator>=(const Fixed &fixed);
+		int operator<=(const Fixed &fixed);
+		int operator==(const Fixed &fixed);
+		int operator!=(const Fixed &fixed);
+
+		float operator+(const Fixed &fixed);
+		float operator-(const Fixed &fixed);
+		float operator*(const Fixed &fixed);
+		float operator/(const Fixed &fixed);
+
+		Fixed operator++();
+		Fixed operator--();
+		Fixed operator++(int);
+		Fixed operator--(int);
+
+		static Fixed &min(Fixed &first, Fixed &second);
+		static const Fixed &min(const Fixed &first, const Fixed &second);
+		static Fixed &max(Fixed &first, Fixed &second);
+		static const Fixed &max(const Fixed &first, const Fixed &second);
+	
+    private:
+        int					_fixed_point_number;
+        static const int	_fractional_bits;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
---
+++ cpp_02/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 16:32:22 by luc               #+#    #+#             */
+/*   Updated: 2026/01/11 20:23:52 by luc              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main()
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return (0);
+}
