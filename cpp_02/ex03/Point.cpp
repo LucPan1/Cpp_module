a1fd5fb8cfb40f0183490992c6a33f4ecb848672
LucPan1 <lucpan2003@gmail.com>
2026-01-14 00:26:31 +0100

    refactor: test and constructor

--- cpp_02/ex03/Point.cpp
+++ cpp_02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 23:08:07 by luc               #+#    #+#             */
-/*   Updated: 2026/01/13 23:17:34 by luc              ###   ########.fr       */
+/*   Updated: 2026/01/14 00:20:52 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 Point::Point(): _x(0), _y(0)
 {
-    std::cout << "Point Default Constructor called" << std::endl;
+    // std::cout << "Point Default Constructor called" << std::endl;
     return;
 }
 
 Point::Point(const float x, const float y): _x(x), _y(y)
 {
-    std::cout << "Point Constructor called" << std::endl;
+    // std::cout << "Point Constructor called" << std::endl;
     return;
 }
 
 Point::Point(const Point& other): _x(other._x), _y(other._y)
 {
-    std::cout << "Point Copy Constructor called" << std::endl;
+    // std::cout << "Point Copy Constructor called" << std::endl;
     *this = other;
     return;
 }
@@ -42,7 +42,7 @@ Point &Point::operator=(const Point& other)
 
 Point::~Point()
 {
-    std::cout << "Point Destructor called" << std::endl;
+    // std::cout << "Point Destructor called" << std::endl;
     return;
 }
 
--- cpp_02/ex03/main.cpp
+++ cpp_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 23:06:40 by luc               #+#    #+#             */
-/*   Updated: 2026/01/14 00:16:36 by luc              ###   ########.fr       */
+/*   Updated: 2026/01/14 00:24:53 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	test_bsp(const char *label, const Point& a, const Point& b, const Point& c, const Point& p)
 {
-	std::cout << ": " << std::endl;
+	std::cout << label << ": " << std::endl;
 
 	if (bsp(a, b, c, p))
 		std::cout << "TRUE" << std::endl;
@@ -24,18 +24,18 @@ void	test_bsp(const char *label, const Point& a, const Point& b, const Point& c,
 
 int main()
 {
-	const Point a(0.0f, 0.0f);
-	const Point b(0.0f, 3.0f);
-	const Point c(4.0f, 0.0f);
+	const Point a(-2.0f, 1.0f);
+	const Point b(3.0f, 4.0f);
+	const Point c(5.0f, -1.0f);
 
-	const Point inside(1.0f, 1.0f);
-	const Point outside(1.0f, 1.0f);
-	const Point edge(1.0f, 1.5f);
-	const Point vertex(0.0f, 0.0f);
+	const Point inside(2.0f, 2.0f);
+	const Point outside(6.0f, 3.0f);
+	const Point edge(0.5f, 2.5f);
+	const Point vertex(3.0f, 4.0f);
 
-	test_bsp("Inside: ", a, b, c, inside);
-	test_bsp("Outside: ", a, b, c, outside);
-	test_bsp("Edge: ", a, b, c, edge);
-	test_bsp("Vertex: ", a, b, c, vertex);
+	test_bsp("Inside ", a, b, c, inside);
+	test_bsp("Outside ", a, b, c, outside);
+	test_bsp("Edge ", a, b, c, edge);
+	test_bsp("Vertex ", a, b, c, vertex);
 	return (0);
 }
