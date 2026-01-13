0bed6e0ff32c46e3038fad8da2d4471ad60e1f27
LucPan1 <lucpan2003@gmail.com>
2026-01-14 00:17:10 +0100

    refactor: test bsp

--- cpp_02/ex03/main.cpp
+++ cpp_02/ex03/main.cpp
@@ -6,18 +6,15 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 23:06:40 by luc               #+#    #+#             */
-/*   Updated: 2026/01/13 23:57:06 by luc              ###   ########.fr       */
+/*   Updated: 2026/01/14 00:16:36 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-void	test_bsp(const Point& a, const Point& b, const Point& c, const Point& p)
+void	test_bsp(const char *label, const Point& a, const Point& b, const Point& c, const Point& p)
 {
-	std::cout << "The point x = " << p.getX() << ", y = " << p.getY() << " is inside the triangle" << std::endl
-			  << "a: x = " << a.getX() << ", y = " << a.getY() << std::endl
-			  << "b: x = " << b.getX() << ", y = " << b.getY() << std::endl
-			  << "c: x = " << c.getX() << ", y = " << c.getY() << std::endl;
+	std::cout << ": " << std::endl;
 
 	if (bsp(a, b, c, p))
 		std::cout << "TRUE" << std::endl;
@@ -27,8 +24,18 @@ void	test_bsp(const Point& a, const Point& b, const Point& c, const Point& p)
 
 int main()
 {
-	test_bsp(Point(), Point(25, 0), Point(0, 25), Point(5, 5));
-	std::cout << "\n--------------------------------------------------\n\n";
-	test_bsp(Point(-32, -32), Point(-5, -5), Point(3.5, 3.5), Point(-9.5, 8.5));
+	const Point a(0.0f, 0.0f);
+	const Point b(0.0f, 3.0f);
+	const Point c(4.0f, 0.0f);
+
+	const Point inside(1.0f, 1.0f);
+	const Point outside(1.0f, 1.0f);
+	const Point edge(1.0f, 1.5f);
+	const Point vertex(0.0f, 0.0f);
+
+	test_bsp("Inside: ", a, b, c, inside);
+	test_bsp("Outside: ", a, b, c, outside);
+	test_bsp("Edge: ", a, b, c, edge);
+	test_bsp("Vertex: ", a, b, c, vertex);
 	return (0);
 }
