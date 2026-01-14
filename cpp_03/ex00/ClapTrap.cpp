32944a8120986b95358133080493d55c86e4a19a
LucPan1 <lucpan2003@gmail.com>
2026-01-14 23:35:46 +0100

    feat: ClapTrap

--- cpp_03/ex00/ClapTrap.cpp
+++ cpp_03/ex00/ClapTrap.cpp
@@ -3,18 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupan <lupan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 15:53:26 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/14 16:04:22 by lupan            ###   ########.fr       */
+/*   Updated: 2026/01/14 23:35:17 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap()
+ClapTrap::ClapTrap(): _name(), _hit_point(10), _energy_point(10), _attack_damage(0)
 {
     std::cout << "ClapTrap Default Constructor called" << std::endl;
+    return;
+}
+
+ClapTrap::ClapTrap(std::string name): _name(name)
+{
+    std::cout << "ClapTrap Constructor called" << std::endl;
     return;
 }
 
@@ -24,7 +30,7 @@ ClapTrap::ClapTrap(const ClapTrap& other)
     return;
 }
 
-ClapTrap &operator=(const ClapTrap& other)
+ClapTrap &ClapTrap::operator=(const ClapTrap& other)
 {
     if (this == &other) {
         return (*this);
@@ -36,4 +42,22 @@ ClapTrap::~ClapTrap()
 {
     std::cout << "ClapTrap Destructor called" << std::endl;
     return;
+}
+
+void    ClapTrap::attack(const std::string& target)
+{
+    this->_energy_point--;
+    std::cout << "ClapTrap " << this->_name << "attacks " << target
+              << ", causing " << this->_attack_damage << " points of damage!"
+              << std::endl;
+}
+
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+    this->_hit_point -= amount;
+}
+
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    this->_hit_point += amount;
 }
--- cpp_03/ex00/main.cpp
+++ cpp_03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupan <lupan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 15:53:34 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/14 16:04:11 by lupan            ###   ########.fr       */
+/*   Updated: 2026/01/14 23:35:08 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int main()
 {
+    ClapTrap claptrap;
+
+    claptrap.attack("clap");
+    claptrap.takeDamage(10);
+    claptrap.beRepaired(20);
     return (0);
 }
