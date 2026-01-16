9fba7d877077558084f53cdf3a8fc1c56f9b4f12
Luc Pan <lupan@f2r7s19.clusters.42paris.fr>
2026-01-16 15:50:42 +0100

    refactor: main output

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lupan <lupan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:54:04 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/15 15:36:50 by lupan            ###   ########.fr       */
+/*   Updated: 2026/01/16 15:49:42 by lupan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int main()
 {
     ScavTrap scavtrap;
 
-    scavtrap.attack("scav");
     scavtrap.takeDamage(10);
+    scavtrap.attack("scav");
     scavtrap.beRepaired(20);
     scavtrap.guardGate();
     return (0);
