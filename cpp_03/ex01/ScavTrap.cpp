33b40bf786ac0cbebba695fe8f07b78a0b4b5787
Luc Pan <lupan@f6r12s4.clusters.42paris.fr>
2026-01-21 16:50:47 +0100

    fix: destruction delegation

--- cpp_03/ex01/ScavTrap.cpp
+++ cpp_03/ex01/ScavTrap.cpp
@@ -6,13 +6,13 @@
 /*   By: lupan <lupan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:55:55 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/21 13:36:21 by lupan            ###   ########.fr       */
+/*   Updated: 2026/01/21 16:24:33 by lupan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap()
+ScavTrap::ScavTrap(): ClapTrap()
 {
     std::cout << "ScavTrap Default Constructor called" << std::endl;
     return;
