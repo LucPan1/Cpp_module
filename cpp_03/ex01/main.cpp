5cd1743af712c16a59e7af6a9e850e0a6a904d2d
LucPan1 <lucpan2003@gmail.com>
2026-01-22 14:03:39 +0100

    feat: add tests

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -6,11 +6,20 @@
 /*   By: luc <luc@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:54:04 by lupan             #+#    #+#             */
-/*   Updated: 2026/01/22 13:35:18 by luc              ###   ########.fr       */
+/*   Updated: 2026/01/22 14:02:09 by luc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+
+void    display_infos(const std::string &name, int hit_point, int energy_point, int attack_damage)
+{
+    std::cout << "Name: " << name << std::endl
+              << "Hit point: " << hit_point << std::endl
+              << "Energy point: " << energy_point << std::endl
+              << "Attack damage: " << attack_damage << std::endl;
+}
 
 int main()
 {
@@ -18,15 +27,12 @@ int main()
     ScavTrap scavtrap("pierre");
 
     claptrap.attack("harry");
-    std::cout << "Name: " << claptrap.getName() << std::endl
-              << "Hit point: " << claptrap.getHitPoint() << std::endl
-              << "Energy point: " << claptrap.getEnergyPoint() << std::endl
-              << "Attack damage: " << claptrap.getAttackDamage() << std::endl;
+    display_infos(claptrap.getName(), claptrap.getHitPoint(), claptrap.getEnergyPoint(), claptrap.getAttackDamage());
 
     claptrap.takeDamage(9);
     std::cout << "Name: " << claptrap.getName() << std::endl
               << "Hit point: " << claptrap.getHitPoint() << std::endl
-              << "Attack damage: " << claptrap.getAttackDamage() << std::endl;
+              << "Attack damage: " << scavtrap.getAttackDamage() << std::endl;
 
     claptrap.beRepaired(20);
      std::cout << "Name: " << claptrap.getName() << std::endl
@@ -34,10 +40,7 @@ int main()
 
     scavtrap.attack("john");
 
-    std::cout << "Name: " << scavtrap.getName() << std::endl
-              << "Hit point: " << scavtrap.getHitPoint() << std::endl
-              << "Energy point: " << scavtrap.getEnergyPoint() << std::endl
-              << "Attack damage: " << scavtrap.getAttackDamage() << std::endl;
+    display_infos(scavtrap.getName(), scavtrap.getHitPoint(), scavtrap.getEnergyPoint(), scavtrap.getAttackDamage());
 
     scavtrap.takeDamage(9);
     std::cout << "Name: " << scavtrap.getName() << std::endl
