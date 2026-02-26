cfae97e51a67d6bd688ca5035953d5740d9d2bad
Luc Pan <lupan@f2r11s14.clusters.42paris.fr>
2026-02-26 15:26:10 +0100

    feat: continue conversion

--- cpp_06/ex00/src/ScalarConverter.cpp
+++ cpp_06/ex00/src/ScalarConverter.cpp
@@ -7,16 +7,28 @@ void ScalarConverter::convert(std::string literal)
     float deci;
     double d;
     char *end;
+    bool verif = false;
 
-    lit = *literal.c_str();
-    num = std::atoi(literal.c_str());
-    deci = std::atof(literal.c_str());
-    d = std::strtod(literal.c_str(), &end);
-    if ((lit >= 'a' && lit <= 'z') || (lit >= 'A' && lit <= 'Z'))
-        std::cout << "char: " << lit << std::endl;
-    else if (lit >= '0' && lit <= '9')
-        std::cout << "char: " << "Non displayable" << std::endl;
-    else
-        std::cout << "char: " << "Impossible" << std::endl;
+    int n = literal.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (literal[i] == 'f')
+        {
+            std::cout << "char: " << "\'*\'" << std::endl;
+            verif = true;
+        }
+    }
+    if (verif == false)
+    {
+        lit = *literal.c_str();
+        num = std::atoi(literal.c_str());
+        deci = std::atof(literal.c_str());
+        d = std::strtod(literal.c_str(), &end);
+        
+        if (lit >= '0' && lit <= '9')
+            std::cout << "char: " << "Non displayable" << std::endl;
+        else if (n > 1 || (lit < 'a' && lit > 'z') || (lit < 'A' && lit > 'Z'))
+            std::cout << "char: " << "Impossible" << std::endl;
+    }
     return;
 }
--- cpp_06/ex00/src/main.cpp
+++ cpp_06/ex00/src/main.cpp
@@ -3,7 +3,7 @@
 int main(int ac, char **av)
 {
     (void)av;
-    if (ac > 2 || ac < 1)
+    if (ac > 2 || ac < 2)
     {
         std::cout << "Wrong number of arguments output" << std::endl;
         return (0);
