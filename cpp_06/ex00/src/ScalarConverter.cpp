ea55a9f640cc21e7fa807ef9f38e17b343f33dbb
lupan <lupan@f1br1s5.paris.42.school>
2026-03-06 12:56:04 +0100

    feat: char and int conversion

--- cpp_06/ex00/src/ScalarConverter.cpp
+++ cpp_06/ex00/src/ScalarConverter.cpp
@@ -4,15 +4,16 @@ void ScalarConverter::convert(const char *literal)
 {
     char *lit;
     int num;
-    // float deci;
+    float deci;
     // double d;
     // char *end;
     bool verif = false;
+    // bool check = false;
 
     lit = const_cast<char *>(literal);
 
     num = std::atoi(literal);
-    // deci = std::atof(literal.c_str());
+    deci = std::atof(literal);
     // d = std::strtod(literal.c_str(), &end);
     int n = strlen(literal);
 	std::string str = lit;
@@ -33,39 +34,44 @@ void ScalarConverter::convert(const char *literal)
             std::cout << "char: impossible" << std::endl;
         else
             std::cout << "char: " << lit << std::endl;
-        // if (!num && num != 0)
-        //     std::cout << "int: impossible" << std::endl;
-        // else 
-        //     std::cout << "int: " << num << std::endl;
-
-        for (int i = 0; i < n; i++)
+        int i = 0;
+        while (i < n)
         {
-            if (literal[i] >= '0' && literal[i] <= '9')
+            if ((literal[i] < '0' || literal[i] > '9') && literal[0] != '-')
+            {
+                i--;
                 std::cout << "int: impossible" << std::endl;
+                break;
+            }
+            i++;
         }
-        if (literal)
-            std::cout << "int: impossible" << std::endl;
-        else 
-            std::cout << "int: " << num << std::endl;
-		// std::cout << "float: " << deci << std::endl;
+        if (i == n)
+        {
+            if (num > 2147483647 || num < -2147483647)
+            {
+                std::cout << "int: impossible" << std::endl;
+            }
+            else 
+                std::cout << "int: " << num << std::endl;
+        }
 
-		// std::cout << "deci: " << typeid(deci).name() << std::endl;
-		// if (deci >= '0' && deci <= '9')
-		// {
-			// std::cout << "float: " << deci << std::endl; 
+		if (deci >= '0' && deci <= '9')
+		{
+			std::cout << "float: " << deci << std::endl; 
 
-			// for (int i = 0; i < n; i++)
-			// {
-			// 	if (i == n - 1)
-			// 		std::cout << "float: " << literal.insert(i + 1, ".0f") << std::endl;
-			// }
-			// std::cout << "hello" << std::endl;
-		// }
-		// else
-		// 	std::cout << "float: nanf" << std::endl; 
-		// if (deci >= '0' && deci <= '9')
+			for (int i = 0; i < n; i++)
+			{
+				if (i == n - 1)
+					std::cout << "float: " << str.insert(i + 1, ".0f") << std::endl;
+			}
+			std::cout << "hello" << std::endl;
+		}
+		else
+			std::cout << "float: nanf" << std::endl; 
 
     }
+        
+        
 
     // return;
 }
