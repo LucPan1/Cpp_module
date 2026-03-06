5d844b24a925f3e35dffb13926019ee41b6d7a4e
lupan <lupan@f1br1s5.paris.42.school>
2026-03-06 16:16:15 +0100

    feat: ex01

---
+++ cpp_06/ex01/includes/Serializer.hpp
@@ -0,0 +1,15 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include <string>
+
+class Serializer {
+
+    public:
+        // static uintptr_t serialize(Data* ptr);
+        // Data* deserialize(uintptr_t raw);
+
+};
+
+#endif
