22cc558db7c617d14ec2f2ee792517607bd4ad60
LucPan1 <lucpan2003@gmail.com>
2026-03-10 00:08:37 +0100

    feat: put constructors in private

--- cpp_06/ex00/includes/ScalarConverter.hpp
+++ cpp_06/ex00/includes/ScalarConverter.hpp
@@ -13,11 +13,13 @@
 class ScalarConverter {
 
     public:
+        static void convert(const std::string& literal);
+    
+    private:
         ScalarConverter();
         ScalarConverter(const ScalarConverter& other);
         ScalarConverter &operator=(const ScalarConverter& other);
         ~ScalarConverter();
-        static void convert(const std::string& literal);
 };
 
 #endif
--- cpp_06/ex01/includes/Serializer.hpp
+++ cpp_06/ex01/includes/Serializer.hpp
@@ -10,14 +10,14 @@
 class Serializer {
 
     public:
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+    
+    private:
         Serializer();
         Serializer(const Serializer& other);
         Serializer &operator=(const Serializer& other);
         ~Serializer();
-
-        static uintptr_t serialize(Data* ptr);
-        static Data* deserialize(uintptr_t raw);
-
 };
 
 #endif
