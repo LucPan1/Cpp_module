ce4307b5fa38d1cb1477e4af275e017eef147c79
Luc Pan <lupan@f4r4s16.clusters.42paris.fr>
2026-03-20 14:52:08 +0100

    feat: test vector and deque

--- cpp_08/ex00/src/main.cpp
+++ cpp_08/ex00/src/main.cpp
@@ -2,8 +2,24 @@
 
 int main()
 {
-    std::vector<int> a = {1, 2, 3, 4, 5};
+    // const std::vector<int> v = {1, 2, 3};
+    static const int arr[] = {1, 2, 3, 4, 5};
+    // int len = 5;
+    // std::deque<int> d1;
+    // for (int i = 1; i < len; i++)
+    //     d1.push_back(i);
 
-    Easyfind<int> s(a, 3);
+    // std::vector<int> v;
+    // for (int i = 1; i < len; i++)
+    //     v.push_back(i);
+    std::vector<int> a(arr, arr + sizeof(arr) / sizeof(int));
+    try
+    {
+        easyfind(a, 3);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     return (0);
 }
