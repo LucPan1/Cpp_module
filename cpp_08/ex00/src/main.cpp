b016dde53823008149647d4ebf4d1dd2c8522edd
Luc Pan <lupan@f4r4s16.clusters.42paris.fr>
2026-03-20 14:52:39 +0100

    refactor: code

--- cpp_08/ex00/src/main.cpp
+++ cpp_08/ex00/src/main.cpp
@@ -2,16 +2,7 @@
 
 int main()
 {
-    // const std::vector<int> v = {1, 2, 3};
     static const int arr[] = {1, 2, 3, 4, 5};
-    // int len = 5;
-    // std::deque<int> d1;
-    // for (int i = 1; i < len; i++)
-    //     d1.push_back(i);
-
-    // std::vector<int> v;
-    // for (int i = 1; i < len; i++)
-    //     v.push_back(i);
     std::vector<int> a(arr, arr + sizeof(arr) / sizeof(int));
     try
     {
